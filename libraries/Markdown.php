9d4fd3203e5b61254fbad05d0bcea4c108bb9d7d
Abbas Adel <abbas.adel@mufix.org>
2013-03-31 15:19:47 +0200

    fix case issue

--- libraries/Markdown.php
+++ libraries/Markdown.php
@@ -4,7 +4,7 @@ class Markdown_Core {
 
     public function parse($text) {
         if (!function_exists('Markdown')) {
-            require Kohana::find_file('vendor', 'markdown');
+            require Kohana::find_file('vendor', 'Markdown');
         }
 
         return Markdown($text);
